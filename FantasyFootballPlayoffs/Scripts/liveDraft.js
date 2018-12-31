7bcb856eb0a038ebefac56599806937312a8f100
Beexiong <bee@partners4results.com>
2018-12-31 16:29:07 -0600

    Live Draft disconnect modification

--- FantasyFootballPlayoffs/Scripts/liveDraft.js
+++ FantasyFootballPlayoffs/Scripts/liveDraft.js
@@ -52,76 +52,90 @@
 
             // Set initial focus to message input box.
             $('#message').focus();
-            // Start the connection.
-            $.connection.hub.start().done(function () {
-                var leagueName = $('#leagueName').html();
-                chat.server.addUserToChatRoom(leagueName);
 
-                //send chat message using interface
-                $('#sendmessage').click(function () {
-                    // Call the Send method on the hub.
-                    chat.server.send($('#displayname').val(), $('#message').val(), leagueName);
-                    // Clear text box and reset focus for next comment.
-                    $('#message').val('').focus();
-                });
-
-                //draft player using interface
-                $('.draftBtn').click(function () {
-                    //Validate that it's user's turn to make selection
-                    var clientUserName = $('#displayname').val();
-                    var userToPickNext = $('#draftBoard').children().first().children().last().text(); // last Grand Child Element to find user name
-                    if (clientUserName === userToPickNext) {
-
-                        //update selection button with new pick, update last pick information, update current user team table
-
-                        //get button element id
-                        var btnid = $(this).prop("id");
-                        //get hidden player id
-                        var playerId = $(this).next().val();
-                        //move to the next element and get hidden details Id
-                        var detailsHidden = $(this).next();
-                        var detailsId = detailsHidden.next().val();
-
-                        //DOM traversing to get parent element (table data) and getting previous siblings
-                        var tdElements = $(this).parent();
-                        tdElements = tdElements.prev();
-                        //gets the value of that table data
-                        var lastPickPosition = tdElements.html();
-                        tdElements = tdElements.prev();
-                        var lastPickTeam = tdElements.html();
-                        tdElements = tdElements.prev();
-                        var lastPickName = tdElements.html();
-                        var lastPickNumber = parseInt($('#lastPickNumber').html()) + 1;
-
-                        //create list of arrays from the team table to pass to function
-                        var userTeam = [];
-                        $('#teamTableInfo tr').each(function () {
-                            var row = [];
-                            $(this).find("td").each(function () {
-                                value = $(this).html();
-                                row.push(value);
-                            });
-                            userTeam.push(row);
-                        });
-
-                        chat.server.receiveSelection(playerId, detailsId, btnid, lastPickName, lastPickTeam, lastPickPosition, lastPickNumber, userTeam, leagueName);
-                    }
-                    else {
-                        //do nothing because incorrect user tried to make selection
-                    }
-                });
-
-                $('#draftOrderBtn').click(function () {
-                    var leagueId = parseInt($('#FantasyLeagueId').val());
-                    var leagueCount = parseInt($('#FantasyLeagueCount').val());
-                    var roomName = $('#leagueName').html();
-
-                    chat.server.determineDraftOrder(leagueCount, leagueId, roomName);
-                });
+            //Start Connection
+            startHub(chat);
+            //Restart Connection on Disconnect
+            $.connection.hub.disconnected(function () {
+                setTimeout(function () {
+                    console.log('Connection disconnected');
+                    startHub(chat)
+                }, 5000); // Restart connection after 5 seconds.
             });
+
         });
 // This optional function html-encodes messages for display in the page.
 function htmlEncode(value) {
     var encodedValue = $('<div />').text(value).html();
     return encodedValue;
+}
+
+function startHub(chat) {
+    // Start the connection.
+    $.connection.hub.start().done(function () {
+        var leagueName = $('#leagueName').html();
+        chat.server.addUserToChatRoom(leagueName);
+
+        //send chat message using interface
+        $('#sendmessage').click(function () {
+            // Call the Send method on the hub.
+            chat.server.send($('#displayname').val(), $('#message').val(), leagueName);
+            // Clear text box and reset focus for next comment.
+            $('#message').val('').focus();
+        });
+
+        //draft player using interface
+        $('.draftBtn').click(function () {
+            //Validate that it's user's turn to make selection
+            var clientUserName = $('#displayname').val();
+            var userToPickNext = $('#draftBoard').children().first().children().last().text(); // last Grand Child Element to find user name
+            if (clientUserName === userToPickNext) {
+
+                //update selection button with new pick, update last pick information, update current user team table
+
+                //get button element id
+                var btnid = $(this).prop("id");
+                //get hidden player id
+                var playerId = $(this).next().val();
+                //move to the next element and get hidden details Id
+                var detailsHidden = $(this).next();
+                var detailsId = detailsHidden.next().val();
+
+                //DOM traversing to get parent element (table data) and getting previous siblings
+                var tdElements = $(this).parent();
+                tdElements = tdElements.prev();
+                //gets the value of that table data
+                var lastPickPosition = tdElements.html();
+                tdElements = tdElements.prev();
+                var lastPickTeam = tdElements.html();
+                tdElements = tdElements.prev();
+                var lastPickName = tdElements.html();
+                var lastPickNumber = parseInt($('#lastPickNumber').html()) + 1;
+
+                //create list of arrays from the team table to pass to function
+                var userTeam = [];
+                $('#teamTableInfo tr').each(function () {
+                    var row = [];
+                    $(this).find("td").each(function () {
+                        value = $(this).html();
+                        row.push(value);
+                    });
+                    userTeam.push(row);
+                });
+
+                chat.server.receiveSelection(playerId, detailsId, btnid, lastPickName, lastPickTeam, lastPickPosition, lastPickNumber, userTeam, leagueName);
+            }
+            else {
+                //do nothing because incorrect user tried to make selection
+            }
+        });
+
+        $('#draftOrderBtn').click(function () {
+            var leagueId = parseInt($('#FantasyLeagueId').val());
+            var leagueCount = parseInt($('#FantasyLeagueCount').val());
+            var roomName = $('#leagueName').html();
+
+            chat.server.determineDraftOrder(leagueCount, leagueId, roomName);
+        });
+    });
 }
